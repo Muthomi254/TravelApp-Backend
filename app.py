f472da20601db0a720ac878a8328ba61132fefee
muthomi254 <dmunene202020@gmail.com>
2024-02-19 13:30:42 +0300

    (ft): Creating app.py Am using python 3.10 Installing Jwt and CORS

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Change this to your database URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your_secret_key'  # Change this to your desired secret key
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+CORS(app)  # Enable CORS for all routes
 
-# Configure the app
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# app.config["JWT_SECRET_KEY"] = "wsddf542455r5rdd55d579j8f6f8yfrd57tru"
+# Define your models here
 
+from auth import auth_blueprint
+from accommodation import accommodation_blueprint
+from travel import travel_blueprint
+
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(accommodation_blueprint)
+app.register_blueprint(travel_blueprint)
+
+# Import your blueprints here
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
