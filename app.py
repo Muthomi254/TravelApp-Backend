7289efb7b75542e19db1d4704f91f6b240517686
machleslie <machariales@gmail.com>
2024-02-19 16:02:29 +0300

    (chore)adding blueprints >Changing some file names and

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -13,13 +14,27 @@ CORS(app)  # Enable CORS for all routes
 
 # Define your models here
 
-from auth import auth_blueprint
-from accommodation import accommodation_blueprint
-from travel import travel_blueprint
+from Blueprints.admin_bp  import admin_bp
+from Blueprints.auth_bp   import auth_bp
+from Blueprints.booking_bp  import booking_bp
+from Blueprints.company_bp import company_bp
+from Blueprints.review_bp import review_bp
+from Blueprints.user_bp import user_bp
+from Blueprints.reservation_bp import reservation_bp
 
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(accommodation_blueprint)
-app.register_blueprint(travel_blueprint)
+
+app.register_blueprint(admin_bp)
+app.register_blueprint(reservation_bp)
+app.register_blueprint(user_bp)
+app.register_blueprint(company_bp)
+app.register_blueprint(review_bp)
+app.register_blueprint(booking_bp)
+app.register_blueprint(auth_bp)
+
+
+
+
+
 
 # Import your blueprints here
 
